48b1a9b5fe7677dd26cac950fad823d9220b0739
hugorocha-web <hjj78092@gmail.com>
2026-07-11 10:08:40 -0300

    atualização

---
+++ 8_objetos/scripts.js
@@ -0,0 +1,20 @@
+var obj={
+    nome: 'hugo',
+    idade: 16,
+    curso: 'informatica'
+}
+console.log(obj)
+console.log(obj.nome)
+console.log(obj.idade)
+/*
+    é como acessar um objeto no poo de python
+*/
+if (obj.idade==16){ 
+    console.log('você tem 16 anos')
+}
+else if (obj.idade==14){
+    console.log('você tem 14 anos')
+}
+else{
+    console.log('você não tem 16 anos')
+}
