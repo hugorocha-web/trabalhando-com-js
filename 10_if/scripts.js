7224dc6da2efd09a1954520012686058a25ba231
hugorocha-web <hjj78092@gmail.com>
2026-07-11 10:54:25 -0300

    ex

---
+++ 10_if/scripts.js
@@ -0,0 +1,11 @@
+var idade=32;
+
+if(idade>=18 && idade <85){
+    console.log('você pode dirigir!')
+}
+else if(idade>=85){
+    console.log('você não pode dirigir mais!')
+}
+else{
+    console.log('você ainda não pode dirigir!')
+}
---
+++ 11_if comparando tipo de dados/scripts.js
@@ -0,0 +1,10 @@
+var numero=3;
+
+
+
+if(numero===2){
+    console.log('numero');
+}
+else if(numero !==2){
+    console.log('é diferente de numero')
+}
