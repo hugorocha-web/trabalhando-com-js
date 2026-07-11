2479e09417c82e92f99da630b1e005fe13c80524
hugorocha-web <hjj78092@gmail.com>
2026-07-11 14:42:37 -0300

    ex

---
+++ 20_funções em strings/scripts.js
@@ -0,0 +1,37 @@
+//toUppercase
+let nome='hugo'
+
+console.log(nome.toUpperCase())
+
+
+
+
+//toLowercase
+nome='HUGO';
+
+console.log(nome.toLowerCase());
+
+
+
+//lenght( igual no python(o tanto de caracteres/objetos dentro))
+let bola='bola';
+
+console.log(bola.length);
+
+//trim (strip()) em python "tira os espaços"
+
+nome='    hugo'
+console.log(nome.trim())
+
+//split (ele corta a frase aonde tiver o que cortar)
+
+console.log(nome.split('u'))
+
+
+
+//index of(ele acha o inicio de quando começa um indice)
+
+//slice(ele pega uma parte da frase depois de um indice que voce colocou)
+//frace.slice(7, 19)
+
+//replace(troca uma palavra de uma str)
